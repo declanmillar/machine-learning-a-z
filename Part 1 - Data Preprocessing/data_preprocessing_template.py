4298e0900ede31e343d638c11adce903963cbbf3
declan-millar <declan.millar@cern.ch>
2019-05-06 18:30:07 +0100

    14

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv("Data.csv")
 # print(dataset.head)
 
 # select all rows for all columns except the last
@@ -18,3 +18,10 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 # print(y)
 
+# Taking care of missing data
+# In statistics, imputation is the process of replacing missing data with substituted values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values=np.nan, strategy="mean", axis=0)
+imputer = imputer.fit(X[:, 1:3]) # upper bound (3) is not included
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+# print(X)
